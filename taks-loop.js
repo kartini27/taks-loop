a8a6f9c09a3144105f46f7beefabbe044f335e4b
kartini <126692640+kartini27@users.noreply.github.com>
2023-03-13 22:29:35 -0700

    Add files via upload

---
+++ taks-loop.js
@@ -0,0 +1,50 @@
+//for loop
+console.log('=========== no 1');
+for(let i =0; i < 10; i++){
+    console.log('halo perkenalkan nama saya kartini situmorang ');
+}
+
+console.log('=========== no 2');
+for(let i = 2; i < 5; i++){
+    console.log('salam kenal yaaa');
+}
+
+// for while
+console.log('======== no 1');
+let i = 14;
+while (i <= 12 ){
+    console.log('saya alumni dari SMA N 1 SITIOTIO');
+    i++;
+}
+
+console.log('======= no 2');
+let i = 10; {
+    console.log('angkatan 2017');
+    i++;
+}
+
+console.log('========= do while 1');
+let i = 15;
+do {
+    console.log('kuliah di universitas quality medan ');
+    i++;
+} while (i <= 10);
+console.log('========= do while 2');
+let i = 5;
+do {
+    console.log(' senang bertemu dengan kalian');
+    i++;
+} while (i <= 4);
+
+// array
+let wadah1 = 'merica';
+let wadah2 = 'minyak';
+let wadah3 = 'gula';
+let wadah4 = 'garam';
+let wadah5 = 'saos';
+let wadah6 = 'lada';
+let wadah = ['merica','minyak','gula','garam','saos','lada'];
+for(let i = 0; i < wadah.length; i++){
+    console.log(wadah[i]);
+}
+
